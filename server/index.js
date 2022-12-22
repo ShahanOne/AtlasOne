f0d3756ed9d8ae27d9260e9649ff22bb2c6fb68f
Shahan.One <shahanhussain9105@gmail.com>
2022-12-22 18:39:48 +0530

    removing dotenv

--- server/index.js
+++ server/index.js
@@ -31,12 +31,9 @@ app.get('/api', (req, res) => {
     });
 });
 
-// let port = process.env.PORT;
-// if (port == null || port == '') {
-//   port = 3001;
-// }
+let port = 3001;
 
-app.listen(3001, function () {
+app.listen(port, function () {
   console.log('Server started on port 3001');
 });
 
