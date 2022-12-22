fbc2c88a9c47e9bc8b324e53633fd985a41db07d
Shahan.One <shahanhussain9105@gmail.com>
2022-12-22 18:22:48 +0530

    removed heroku

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
-require('dotenv').config();
-const express = require("express");
+const express = require('express');
 const app = express();
 const path = require('path');
 
@@ -9,52 +8,47 @@ const CircularJSON = require('circular-json');
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-
 // All other GET requests not handled before will return our React app
 //  app.get('/', (req, res) => {
 //    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 //  });
 
-app.get("/api", (req,res)=>{
-const hero = [{"haha":"hoho"},{"hey":"lmo"}];
-    const url = "https://restcountries.com/v2/all";
+app.get('/api', (req, res) => {
+  const hero = [{ haha: 'hoho' }, { hey: 'lmo' }];
+  const url = 'https://restcountries.com/v2/all';
 
-    axios.get(url)
+  axios
+    .get(url)
     .then(function (response) {
-        let json = CircularJSON.stringify(response.data);
-        res.send(json);
+      let json = CircularJSON.stringify(response.data);
+      res.send(json);
       // handle success
-    //   res.json(response)
+      //   res.json(response)
     })
     .catch(function (error) {
       // handle error
       console.log(error);
-    })
-
-})
-
-
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port =3001;
-}
-
-app.listen(port, function() {
-  console.log("Server started on port 3001");
+    });
 });
 
+let port = process.env.PORT;
+if (port == null || port == '') {
+  port = 3001;
+}
 
+app.listen(port, function () {
+  console.log('Server started on port 3001');
+});
 
 // https.get(url , function (response) {
-   
+
 //     response.on("data" , function (data) {
-     
+
 //       const countryData = data.toString();
 
 //       // const simpleCountry = JSON.string(countryData);
 
 //       res.json({"countryArray": countryData });
- 
+
 //     })
 //   })
