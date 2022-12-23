106aaa91a6d2ef86b4562fe79e61a88edfc19df3
Shahan.One <shahanhussain9105@gmail.com>
2022-12-24 02:47:02 +0530

    remove /build link

--- server/index.js
+++ server/index.js
@@ -20,9 +20,9 @@ const CircularJSON = require('circular-json');
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // All other GET requests not handled before will return our React app
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
 
 app.get('/api', (req, res) => {
   const hero = [{ haha: 'hoho' }, { hey: 'lmo' }];
